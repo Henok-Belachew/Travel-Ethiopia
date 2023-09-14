198e5ff94f38a23f7a57703f2fd8ae79408297b4
Henok Belachew <henokbelachewstu@gmail.com>
2023-09-14 18:30:13 +0300

    font updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className='flex justify-between gap-10  py-[40px] px-96'>
         <div className=' w-1/4 flex flex-col gap-2'>
             <img src={Logo} className='w-[70%] block' alt="" />
-            <span className='leading-[18px] opacity-70 text-sm'>
+            <span className='leading-[18px] opacity-70 text-[16px]'>
             Stay connected with us through our social media channels.
 
             </span>
@@ -23,7 +23,7 @@ function Footer() {
         </div>
         <div className=' w-1/4'>
           <h1 className='font-bold'>Discover</h1>
-          <ul className='mt-5 opacity-70 flex flex-col gap-2 text-sm'>
+          <ul className='mt-5 opacity-70 flex flex-col gap-2 text-[16px]'>
             <li className='hover:font-bold cursor-pointer'>Home</li>
             <li className='hover:font-bold cursor-pointer'>Tour</li>
             <li className='hover:font-bold cursor-pointer'>About</li>
@@ -33,7 +33,7 @@ function Footer() {
         </div>
         <div className='w-1/4'>
         <h1 className='font-bold'>Quick Links</h1>
-          <ul className='mt-5 opacity-70 flex flex-col gap-2 text-sm'>
+          <ul className='mt-5 opacity-70 flex flex-col gap-2 text-[16px]'>
             <li className='hover:font-bold cursor-pointer'>Gallery</li>
             <li className='hover:font-bold cursor-pointer'>Login</li>
             <li className='hover:font-bold cursor-pointer'>Register</li>
@@ -44,24 +44,24 @@ function Footer() {
 
           <div className='mt-5 flex flex-col gap-1'>
               <div className='flex gap-4 items-center'>
-                <span className='font-semibold flex gap-2 items-center text-sm'>
+                <span className='font-semibold flex gap-2 items-center text-[16px]'>
                 <IoLocationOutline className='text-orange-500'/>
                 Address:
                 </span>
 
-                <span className='text-sm opacity-70'>
+                <span className='text-[16px] opacity-70'>
                   Addis Ababa, Ethiopia
                 </span>
                 
               </div>
 
               <div className='flex gap-4 items-center'>
-                <span className='font-semibold flex gap-2 items-center text-sm'>
+                <span className='font-semibold flex gap-2 items-center text-[16px]'>
                 <AiOutlinePhone className='text-orange-500'/>
                 Phone:
                 </span>
 
-                <span className='text-sm opacity-70'>
+                <span className='text-[16px] opacity-70'>
                   0115534453
                 </span>
                 
