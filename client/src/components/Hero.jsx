335f5ea6da7ae0da3e129a100c544abbb2f1911d
Henok Belachew <henokbelachewstu@gmail.com>
2023-09-14 21:25:55 +0300

    hero section searching inputs is added

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import img1 from '../assets/images/adj-1.jpg'
 import img2 from '../assets/images/adj-2.jpg'
 import img3 from '../assets/images/adj-3.jpg'
+import { IoLocationOutline } from 'react-icons/io5'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {GiPathDistance} from 'react-icons/gi'
 
 function Heron() {
   return (
@@ -15,6 +18,32 @@ function Heron() {
             <p className='w-[80%] opacity-70'>
             Immerse yourself in a land where ancient history meets vibrant culture, where breathtaking landscapes and warm hospitality await your journey of discovery.
             </p>
+
+            <div className='shadow-lg p-3 gap-1 flex items-center justify-between rounded-lg w-[85%]'>
+                <div className='flex items-center'>
+                  {/* Icon here */}
+                  <IoLocationOutline className='w-[40px] font-bold text-orange-500 text-2xl'/>
+                  <div className='flex flex-col'>
+                    <span className='font-semibold opacity-70'>Location</span>
+                    <input type="text" placeholder='Place to go ?' className='text-sm focus:outline-none py-' />
+                  </div>
+                </div>
+
+                <div className='w-[1px] h-[44px] bg-black opacity-40'></div>
+
+                <div className='flex items-center'>
+                  {/* Icon here */}
+                  <GiPathDistance className='w-[40px] font-bold text-orange-500 text-2xl'/>
+                  <div className='flex flex-col'>
+                    <span className='font-semibold opacity-70'>Distance</span>
+                    <input type="text" placeholder='Distance in Km' className='text-sm focus:outline-none py-' />
+                  </div>
+                </div>
+
+                <button className='bg-orange-500 p-3 self-stretch rounded-lg text-white'>
+                      <AiOutlineSearch/>
+                </button>
+            </div>
 
         </div>
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
         <div className='relative block mx-auto w-[320px]'>
         <img src={Logo} className='w-[70%] mx-auto mb-6' alt="" />
         <h1 className='font-bold text-2xl'>Welcome!</h1>
-        <span>Log in to continue</span>
+        <span>Log in to continue.</span>
 
             <div className='mt-4 flex flex-col gap-4'>
             <div className='flex gap-1 justify-center flex-col'>
@@ -20,10 +20,11 @@ function Login() {
             <div className='flex gap-1 justify-center flex-col'>
                 <span className='font-semibold'>Password</span>
                 <input placeholder='Password ...' className='focus:outline-none border-solid text-sm border-light-grey border-[1px] px-2 py-2 rounded-md' type="password" />
-                <span className='text-xs self-end opacity-70'>Forgot Password ?</span>
+                <span className='text-xs self-end opacity-70 cursor-pointer hover:font-semibold hover:opacity-100'>Forgot Password ?</span>
             </div>
 
-            <button className='bg-orange-200 opacity-100 text-white hover:opacity-100 font-semibold hover:bg-orange-500 hover:text-white mt-4 py-2 rounded-lg'>Log In</button>
+            <button className='bg-black opacity-100 text-white hover:opacity-100 font-semibold hover:bg-orange-500 hover:text-white mt-4 py-2 rounded-lg'>Log In</button>
+            <span className='self-center text-sm op'>Don't have account ? <span className='font-semibold text-orange-500 cursor-pointer opacity-100'> <span className='opacity-70 hover:opacity-100 hover:font-bold'>Register</span> </span> </span>
 
             </div>
 
