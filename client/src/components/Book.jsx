1f1701bd13390ecc38202db9c296444d6ae4bc77
Henok Belachew <henokbelachewstu@gmail.com>
2023-09-24 11:51:08 +0300

    Booking form updated! Now it's ready for demo!

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -12,15 +12,15 @@ function Book() {
          <img src={img} className='rounded-lg w-[58%] h-[385px] object-cover' alt="" />
 
          {/* Right Side */}
-         <div className='w-[40%] flex flex-col p-6 border-slid shadow-sm border-[1px] rounded-lg gap-6'>
+         <div className='w-[40%] flex flex-col p-6 border-slid border-[1px] bg-green-50 border-green-700 rounded-lg gap-6'>
 
             {/* Top Section */}
             <div className='flex flex-col gap-5'>
                <div className='flex justify-between'>
-               <h2 className='font-'>Pricing <span className='font-bold text-green-500'>1,499 <span className='font-light text-xs'>ETB</span> </span> </h2>
+               <h2 className='font-semibold'>Pricing <span className='font-bold text-green-700'>1,499 <span className='font-light text-xs'>ETB</span> </span> </h2>
 
                   <div className='flex gap-1 items-center'>
-                     Rating <span className='font-semibold text-green-500 -mr-1'>3</span> <AiFillStar className='text-green-500' />
+                     Rating <span className='font-semibold text-green-700 -mr-1'>3</span> <AiFillStar className='text-green-700' />
                   </div>
 
                </div>
@@ -28,7 +28,7 @@ function Book() {
 
                {/* <div className='border-t-[1px] border-solid border-black'>1</div> */}
 
-               <hr  className='border-t-[0.7px] border-solid' />
+               <hr  className='border-t-[0.7px] border-green-700 border-solid' />
                
             </div>
 
@@ -36,22 +36,21 @@ function Book() {
                <h1 className='font-semibold text-[16px] '>Information</h1>
 
                {/* Input Fields */}
-               <div className='flex flex-col justify-between gap-2 px-3 pb-3 border-solid border-[1px] rounded-md'>
-                  <input placeholder='Full Name ...' type="text" className='focus:outline-none p-3 border-b-[1px] ' />
-                  <input placeholder='Phone number' type="number" className='focus:outline-none p-3 border-b-[1px] ' />
-                  <div className='flex gap-1'>
-                     <input type="date" placeholder='Choose Date' className='w-1/2 focus:outline-none border-b-[1px] p-2 ' />
-                     <input placeholder='Guests' type="number" className='w-1/2 focus:outline-none p-3 border-b-[1px] ' />
-                  </div>
+               <div className='flex flex-col justify-between gap-4 p-6 bg-white  border-green-600 border-solid border-[1px] rounded-md'>
+                  <label className='font-semibold flex flex-col gap-2'> Full Name <input placeholder='Enter your full name ...' type="text" className='font-normal border-green-700 focus:outline-none  text-sm border-b-[1px] py-1' /> </label>
+                  <label className='font-semibold flex flex-col gap-2'> Phone Number <input placeholder='+251 ...' type="text" className='font-normal border-green-700  focus:outline-none text-sm border-b-[1px] py-1' /> </label>
+                  <label className='font-semibold  flex flex-col gap-3'>No of Guests <input placeholder='Enter total passengers here ...' type="number" className='font-normal py-1  border-b-[1px] focus:outline-none border-green-700 text-sm ' /> </label>
+                  <label className='font-semibold flex items-center gap-2'> Date <input placeholder='+251 ...' type="date" className='font-normal border-green-700  focus:outline-none text-sm ' /> </label>
+                 
                </div>
 
                {/* Invoice Section */}
                <div className='px-2 flex flex-col gap-2'>
-                  <h1 className='flex justify-between text-sm opacity-60'>
+                  <h1 className='flex justify-between text-md opacity-80'>
                      <span>$199 X 1 Person</span>
                      <span>$199</span>
                   </h1>
-                  <h1 className='flex justify-between text-sm opacity-60'>
+                  <h1 className='flex justify-between text-md opacity-80'>
                      <span>Service Charge</span>
                      <span>$20</span>
                   </h1>
@@ -61,7 +60,7 @@ function Book() {
                   </h1>
                </div>
 
-               <button className='py-3 bg-green-500 rounded-md font-semibold text-white'>Book Now</button>
+               <button className='py-3 bg-green-300 hover:bg-green-500  hover:text-white rounded-md font-semibold text-green-700'>Book Now</button>
 
 
             </div>
