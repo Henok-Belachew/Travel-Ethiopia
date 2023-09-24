a67be66f560e234348fac6ae9fbaf695211d15f4
Henok Belachew <henokbelachewstu@gmail.com>
2023-09-24 12:11:40 +0300

    Final booking form updating made

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -9,18 +9,18 @@ function Book() {
       <div className='flex justify-between'>
 
          {/* Left Side */}
-         <img src={img} className='rounded-lg w-[58%] h-[385px] object-cover' alt="" />
+         <img src={img} className='rounded-lg w-[63%] h-[385px] object-cover' alt="" />
 
          {/* Right Side */}
-         <div className='w-[40%] flex flex-col p-6 border-slid border-[1px] bg-green-50 border-green-700 rounded-lg gap-6'>
+         <div className='w-[35%] flex flex-col p-6 border-slid border-[1px]  rounded-lg gap-6'>
 
             {/* Top Section */}
             <div className='flex flex-col gap-5'>
                <div className='flex justify-between'>
-               <h2 className='font-semibold'>Pricing <span className='font-bold text-green-700'>1,499 <span className='font-light text-xs'>ETB</span> </span> </h2>
+               <h2 className='font-semibold'> <span className='font-bold text-3xl text-green-700'>1,499 <span className='font-light text-xs'>ETB/person</span> </span> </h2>
 
                   <div className='flex gap-1 items-center'>
-                     Rating <span className='font-semibold text-green-700 -mr-1'>3</span> <AiFillStar className='text-green-700' />
+                     Rating <span className='font-semibold text-green-700 -mr-1'>3 (2) </span> <AiFillStar className='text-green-700' />
                   </div>
 
                </div>
@@ -28,7 +28,7 @@ function Book() {
 
                {/* <div className='border-t-[1px] border-solid border-black'>1</div> */}
 
-               <hr  className='border-t-[0.7px] border-green-700 border-solid' />
+               <hr  className='border-t-[0.7px]  border-solid' />
                
             </div>
 
@@ -36,11 +36,11 @@ function Book() {
                <h1 className='font-semibold text-[16px] '>Information</h1>
 
                {/* Input Fields */}
-               <div className='flex flex-col justify-between gap-4 p-6 bg-white  border-green-600 border-solid border-[1px] rounded-md'>
-                  <label className='font-semibold flex flex-col gap-2'> Full Name <input placeholder='Enter your full name ...' type="text" className='font-normal border-green-700 focus:outline-none  text-sm border-b-[1px] py-1' /> </label>
-                  <label className='font-semibold flex flex-col gap-2'> Phone Number <input placeholder='+251 ...' type="text" className='font-normal border-green-700  focus:outline-none text-sm border-b-[1px] py-1' /> </label>
-                  <label className='font-semibold  flex flex-col gap-3'>No of Guests <input placeholder='Enter total passengers here ...' type="number" className='font-normal py-1  border-b-[1px] focus:outline-none border-green-700 text-sm ' /> </label>
-                  <label className='font-semibold flex items-center gap-2'> Date <input placeholder='+251 ...' type="date" className='font-normal border-green-700  focus:outline-none text-sm ' /> </label>
+               <div className='flex flex-col justify-between gap-4 p-6  border-solid border-[1px] rounded-md'>
+                  <label className='font-semibold flex flex-col gap-2'> Full Name <input placeholder='Enter your full name ...' type="text" className='font-normal  border-green-700 focus:outline-none  text-sm border-b-[1px] py-1' /> </label>
+                  <label className='font-semibold flex flex-col gap-2'> Phone Number <input placeholder='+251 ...' type="text" className='font-normal  border-green-700  focus:outline-none text-sm border-b-[1px] py-1' /> </label>
+                  <label className='font-semibold  flex flex-col gap-3'>No of Guests <input placeholder='Enter total passengers here ...' type="number" className='font-normal  py-1  border-b-[1px] focus:outline-none border-green-700 text-sm ' /> </label>
+                  <label className='font-semibold flex items-center gap-2'> Date <input placeholder='+251 ...' type="date" className='font-normal  border-green-700  focus:outline-none text-sm ' /> </label>
                  
                </div>
 
@@ -60,7 +60,7 @@ function Book() {
                   </h1>
                </div>
 
-               <button className='py-3 bg-green-300 hover:bg-green-500  hover:text-white rounded-md font-semibold text-green-700'>Book Now</button>
+               <button className='py-3 bg-green-100 border-[1px] border-green-700 hover:bg-green-500  hover:text-white rounded-md font-semibold text-black'>Book Now</button>
 
 
             </div>
